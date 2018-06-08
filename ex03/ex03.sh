59869dafbe34b3e7b53bbd0322c6910114f3f369
Vlad <vlad_pavlik@ukr.net>
2018-06-08 15:11:25 +0300

    ex3 is finished

---
+++ ex03/ex03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$#" -eq 4 ];
+then
+  first_arg=$1
+  shift
+  for arg in $@; do
+    echo "$arg" $(grep -o $arg $first_arg | wc -l)
+    grep -n $arg $first_arg | cut --delimiter=':' -f 1
+  done
+else
+  exit 1
+fi
